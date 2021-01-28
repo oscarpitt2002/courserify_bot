448cc13dc22f295f543539d67b1c5ac60161ef87
oscarpitt2002 <76811244+oscarpitt2002@users.noreply.github.com>
2021-01-28 18:55:28 +0530

    Update course.js

--- course.js
+++ course.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const findCourses = (ctx, args) => {
     const query = args.filter(a => a.indexOf('about=') > -1)[0] || '';
     const language = args.filter(a => a.indexOf('lang=') > -1)[0] || '';
-    fetch(`https://www.udemy.com/api-2.0/courses/?search=${query.replace('about=', '')}&language=${language.replace('lang=', '')}&page_size=50&ordering=newest&price=price-free&ratings=3`, {
+    fetch(`https://www.udemy.com/api-2.0/courses/?search=${query.replace('about=', '')}&language=${language.replace('lang=', '')}&page_size=50&ordering=highest-rated&price=price-free`, {
         headers: {
             "Accept": "application/json, text/plain, */*",
             "Authorization": `Basic Sk4yY3g4RUROTjhEMEhqZnA1dllUMXNEU044b1cySWw3dzE5MFNXRDpwZ04zY0liWG0zbUo1VTZEWGpKcFNJeXFEM2JCbGRjTGpVZ2dKU2dtMmZMVFFsMTE2Q2paY2RaMVNHYlBKcTE3Z2ZDY1c5eWpKYzBtVmdNVlNsQjE1WDVRU1d4ZGpCVmdOUUY4NTFFYUxsV2ttb3ZnY2JGZ0pKMFRPRXJ0RzV1eg==`,
