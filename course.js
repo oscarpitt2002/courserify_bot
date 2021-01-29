d1522bfe5e37426d0a89ded0e65bde1ce6de5335
oscarpitt2002 <76811244+oscarpitt2002@users.noreply.github.com>
2021-01-29 20:57:08 +0530

    Update course.js

--- course.js
+++ course.js
@@ -3,8 +3,8 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 const findCourses = (ctx, args) => {
-    const query = args.filter(a => a.indexOf('about=') > -1)[0:10] || '';
-    const language = args.filter(a => a.indexOf('lang=') > -1)[0:10] || '';
+    const query = args.filter(a => a.indexOf('about=') > -1)[0..10] || '';
+    const language = args.filter(a => a.indexOf('lang=') > -1)[0..10] || '';
     fetch(`https://www.udemy.com/api-2.0/courses/?search=${query.replace('about=', '')}&language=${language.replace('lang=', '')}&page_size=50&ordering=highest-rated&price=price-free`, {
         headers: {
             "Accept": "application/json, text/plain, */*",
